85e295d9909980b946d6bb1f41e6ff7c1a0eb97f
Terry Tosh <tmt103@txstate.edu>
2020-09-18 04:30:20 +0000

    ReverseArray()

--- hw3.cpp
+++ hw3.cpp
@@ -1,12 +1,26 @@
 /****
 * PSEUDOCODE:
+ReverseArray():
+
 *
 */
+#include <iostream>
 #include <string>
 #include "hw3.h"
+using std::cout;
+using std::endl;
 
 void ReverseArray(int arr[], int length){
-
+  int *temp_arr = new int(length);
+  int ctr = length - 1;
+  for(int i = 0; i < length; i++){
+    temp_arr[i] = arr[ctr];
+    ctr--;
+  }
+  for(int j = 0; j < length; j++){
+    arr[j] = temp_arr[j];
+  }
+  delete temp_arr;
 }
 
 bool HasBalancedParentheses(std::string statement){
